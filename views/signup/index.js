41c94f5cfacd77a4ed84d2a15f38326bed5224be
Rafael Trias <rafatrias.27@gmail.com>
2025-07-03 15:41:15 -0400

    update index.js

--- views/signup/index.js
+++ views/signup/index.js
@@ -1,8 +1,8 @@
 const form = document.querySelector('#form');
-const nameInput = document.querySelector('name-input');
-const emailInpunt = document.querySelector('#email-input');
+const nameInput = document.querySelector('#name-input');
+const emailInput = document.querySelector('#email-input');
 const passwordInput = document.querySelector('#password-input');
-const matchImput = document.querySelector('#match-input');
+const matchInput = document.querySelector('#match-input');
 
 // Regex validations
 const EMAIL_VALIDATION = /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/;
@@ -12,5 +12,20 @@ const NAME_VALIDATION = /^[A-Z][a-zA-Z-ÿ\u00f1\u00d1]+(\s*[A-Z][a-zA-Z-ÿ\u00f1
 // Events
 
 nameInput.addEventListener('input', e => {
-    console.log(e.target.value); 
+    const nameValidation = NAME_VALIDATION.test(e.target.value);
+
+    if (e.target.value === '') {
+        nameInput.classList.remove('focus:outline-indigo-700');
+        nameInput.classList.remove('focus:outline-green-700');
+        nameInput.classList.add('focus:outline-red-700');
+    }
+
+    if (nameValidation) {
+        nameInput.classList.remove('focus:outline-indigo-700');
+        nameInput.classList.add('focus:outline-green-700');
+    } else {
+        nameInput.classList.remove('focus:outline-indigo-700');
+        nameInput.classList.remove('focus:outline-green-700');
+        nameInput.classList.add('focus:outline-red-700');
+    }
 });
