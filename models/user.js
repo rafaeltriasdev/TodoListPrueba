4cb20dcf8fa437ab94c5125bfd3cc86114cecff5
Rafael Trias <rafatrias.27@gmail.com>
2025-07-04 10:45:47 -0400

    add user.js controller and modify user model

---
+++ models/user.js
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose'); // Importa mongoose para interactuar con MongoDB
+
+// Define el esquema del modelo de usuario
+const userSchema = new mongoose.Schema({ 
+    name: String, // Almacena el nombre del usuario
+    email: String, // Almacena el correo electrónico del usuario
+    passwordHash: String, // Almacena el hash de la contraseña
+    verified: { 
+     type: Boolean,
+     default: false
+     }, // Indica si el usuario ha verificado su cuenta
+});
+
+// Configura cómo se transforman los documentos al convertirlos a JSON
+userSchema.set('toJSON', {
+    transform: (document, returnedObject) => {
+        returnedObject.id = returnedObject._id.toString(); // Convierte el campo _id a un string y lo renombra a id
+        delete returnedObject._id; // Elimina el campo _id que es generado por MongoDB
+        delete returnedObject.__v; // Elimina el campo __v que es usado por Mongoose para el control de versiones
+        delete returnedObject.passwordHash; // No enviar el hash de la contraseña
+    }
+});
+
+// Crea el modelo de usuario basado en el esquema definido
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
