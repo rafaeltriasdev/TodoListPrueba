dbd6523b3b33f254846e294c75d44af2728ef94f
Rafael Trias <rafatrias.27@gmail.com>
2025-07-03 21:10:19 -0400

    update validations index.js

--- views/signup/index.js
+++ views/signup/index.js
@@ -5,27 +5,48 @@ const passwordInput = document.querySelector('#password-input');
 const matchInput = document.querySelector('#match-input');
 
 // Regex validations
-const EMAIL_VALIDATION = /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/;
-const PASSWORD_VALIDATION = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{6,8}$/;
-const NAME_VALIDATION = /^[A-Z][a-zA-Z-ÿ\u00f1\u00d1]+(\s*[A-Z][a-zA-Z-ÿ\u00f1\u00d1]*)$/;
+const EMAIL_VALIDATION = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+const PASSWORD_VALIDATION =  /^(?=.*\d)(?=.*[a-z])(?=.*[a-zA-Z]).{8,16}$/;
+const NAME_VALIDATION = /^[A-Z\u00d1][a-zA-Z-ÿáéíóú\u00f1\u00d1]+(\s*[A-Z\u00d1][a-zA-Z-ÿáéíóú\u00f1\u00d1\s]*)$/;
+
+// Validations 
+let nameValidation = false;
+let emailValidation = false;
+let passwordValidation = false;
+let matchValidation = false;
+
+const validation = (input, regexValidation) => {
+    if (input.value === '') {
+        input.classList.remove('outline-red-700', 'outline-1', 'outline');
+        input.classList.remove('outline-green-700', 'outline-1', 'outline');
+        input.classList.add('focus:outline-indigo-700');
+    } else if (regexValidation) {
+        input.classList.remove('focus:outline-indigo-700');
+        input.classList.add('outline-green-700', 'outline-1', 'outline');
+    } else if (!regexValidation) {
+        input.classList.remove('focus:outline-indigo-700');
+        input.classList.remove('outline-green-700', 'outline-1', 'outline');
+        input.classList.add('outline-red-700', 'outline-1', 'outline');
+    }
+};
 
 // Events
-
 nameInput.addEventListener('input', e => {
-    const nameValidation = NAME_VALIDATION.test(e.target.value);
+    nameValidation = NAME_VALIDATION.test(e.target.value);
+    validation(nameInput, nameValidation);
+});
 
-    if (e.target.value === '') {
-        nameInput.classList.remove('focus:outline-indigo-700');
-        nameInput.classList.remove('focus:outline-green-700');
-        nameInput.classList.add('focus:outline-red-700');
-    }
+emailInput.addEventListener('input', e => {
+    emailValidation = EMAIL_VALIDATION.test(e.target.value);
+    validation(emailInput, emailValidation);
+});
 
-    if (nameValidation) {
-        nameInput.classList.remove('focus:outline-indigo-700');
-        nameInput.classList.add('focus:outline-green-700');
-    } else {
-        nameInput.classList.remove('focus:outline-indigo-700');
-        nameInput.classList.remove('focus:outline-green-700');
-        nameInput.classList.add('focus:outline-red-700');
-    }
+passwordInput.addEventListener('input', e => {
+    passwordValidation = PASSWORD_VALIDATION.test(e.target.value);
+    validation(passwordInput, passwordValidation);
+});
+
+matchInput.addEventListener('input', e => {
+    matchValidation = MATCH_VALIDATION.test(e.target.value);
+    validation(matchInput, matchValidation);
 });
