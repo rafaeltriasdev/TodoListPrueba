d376ae34093415448ee6855e8ba5c41f894e1fa9
diegoalavarez <diegoaam16.15@gmail.com>
2025-07-08 02:37:48 -0400

    update auht.js

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,13 +1,22 @@
 const jwt = require('jsonwebtoken');
 
 const userExtractor = async (req, res, next) => {
-    const token = req.cookies?.accessToken;
+    try{
+        const token = req.cookies?.accessToken;
     if (!token) {
-        return res.sendStatus(401);
+        res.sendStatus(401);
     }
-    
-    const decdoded = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
+
+    const badToken = jwt.sign('jajajajaaaa', 'hola');
+
+    const decoded = jwt.verify(badToken, process.env.ACCESS_TOKEN_SECRET);
+    console.log(decoded);
+    } catch (error) {
+        res.sendStatus(403); // Forbidden if token is invalid
+        
+    }
     next();
 };
+
 
 module.exports = { userExtractor };
