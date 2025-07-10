1942b83e82cf1fe8cfd2ce0ace2553f99b8c993e
diegoalavarez <diegoaam16.15@gmail.com>
2025-07-09 23:29:28 -0400

    update login.js

--- controllers/login.js
+++ controllers/login.js
@@ -7,6 +7,7 @@ const jwt = require('jsonwebtoken');
 loginRouter.post('/' , async (request, response) => {
     const { email, password } = request.body;
     const userExist = await User.findOne({ email });
+    // console.log(email, password);
     
     if (!userExist) {
         return response.status(400).json({ error: 'Email o contraseña incorrectos' });
@@ -16,7 +17,7 @@ loginRouter.post('/' , async (request, response) => {
         return response.status(400).json({ error: 'Email no verificado' });
     }
 
-    const isCorrect = await bcrypt.compare(password, userExist.passwordHash);
+    const isCorrect = await bcrypt.compare(password, userExist.passwordHash); //repasar
     
     if (!isCorrect) {
         return response.status(400).json({ error: 'Email o contraseña incorrectos' });
@@ -26,7 +27,7 @@ const userForToken = {
     id: userExist.id,
 }
 
-const accessToken = jwt.sign(userForToken, process.env.ACCESS_TOKEN_SECRET, {
+const accessToken = jwt.sign(userForToken, process.env.ACCESS_TOKEN_SECRET, { //repasar
     expiresIn: '1d'
 });
 
@@ -38,6 +39,8 @@ response.cookie('accessToken', accessToken, {
 });
 
 return response.sendStatus(200);
+
+
 
 });
 
