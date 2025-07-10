d4b92ddc909715224a50a67c64e6f65a1f5bc4ab
diegoalavarez <diegoaam16.15@gmail.com>
2025-07-09 23:33:05 -0400

    update class

--- middleware/auth.js
+++ middleware/auth.js
@@ -4,7 +4,7 @@ const User = require('../models/user');
 
 const userExtractor = async (request, response, next) => { 
     // console.log(request.cookies.accessToken);
-    try{
+    try {
         const token = request.cookies?.accessToken;
     if (!token) {
         return response.sendStatus(401);
@@ -13,7 +13,7 @@ const userExtractor = async (request, response, next) => {
     const decoded = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
     const user = await User.findById(decoded.id);
     request.user = user; // Attach the user to the request object
-    } 
+    }
     
     catch (error) {
     return response.sendStatus(403); // Forbidden if token is invalid
